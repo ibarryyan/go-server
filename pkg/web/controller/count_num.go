9e9e4022aa84dddef580d70df341df7556f399b9
yanmx <1712229564@qq.com>
2022-06-14 09:47:56 +0800

    Web层初步功能完成

--- pkg/web/controller/count_num.go
+++ pkg/web/controller/count_num.go
@@ -2,8 +2,11 @@ package controller
 
 import (
 	"count_num/pkg/dao/impl"
-	"fmt"
+	"count_num/pkg/entity"
+	"encoding/json"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"strconv"
 )
 
 type NumInfoControllerImpl struct {
@@ -24,30 +27,38 @@ func NewNumInfoControllerImpl() *NumInfoControllerImpl {
 
 func (impl NumInfoControllerImpl) AddNumByKey(c *gin.Context) {
 	key := c.Param("key")
-	fmt.Println(impl.dao)
-	c.JSON(200, key)
+	numInfo := impl.dao.GetNumInfoByKey(c, key)
+	numInfo.InfoNum = numInfo.InfoNum + 1
+	isOk := impl.dao.UpdateNumInfoByKey(c, numInfo)
+	c.JSON(200, isOk)
 }
 
 func (impl NumInfoControllerImpl) FindNumByKey(c *gin.Context) {
 	key := c.Param("key")
-
-	c.JSON(200, key)
+	numInfo := impl.dao.GetNumInfoByKey(c, key)
+	c.JSON(200, numInfo)
 }
 
 func (impl NumInfoControllerImpl) SaveNumInfo(c *gin.Context) {
-	key := c.Param("key")
-
-	c.JSON(200, key)
+	body := c.Request.Body
+	bytes, err := ioutil.ReadAll(body)
+	info := entity.NumInfo{}
+	json.Unmarshal(bytes, &info)
+	if err != nil {
+		panic(err)
+	}
+	isOk := impl.dao.AddNumInfo(c, info)
+	c.JSON(200, isOk)
 }
 
 func (impl NumInfoControllerImpl) DeleteById(c *gin.Context) {
-	key := c.Param("key")
-
-	c.JSON(200, key)
+	id := c.Param("id")
+	i, _ := strconv.Atoi(id)
+	isOk := impl.dao.DeleteNumInfoById(c, int64(i))
+	c.JSON(200, isOk)
 }
 
 func (impl NumInfoControllerImpl) FindAll(c *gin.Context) {
-	key := c.Param("key")
-
-	c.JSON(200, key)
+	numInfos := impl.dao.FindAllNumInfo(c)
+	c.JSON(200, numInfos)
 }
