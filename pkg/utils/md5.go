e0a0a3b920e284562fe05fd05f5cc2ddf18353e5
yanmx <1712229564@qq.com>
2022-07-03 21:01:54 +0800

    add token

--- pkg/utils/md5.go
+++ pkg/utils/md5.go
@@ -5,6 +5,9 @@ import (
 	"crypto/md5"
 	"encoding/gob"
 	"encoding/hex"
+	"math/rand"
+	"strings"
+	"time"
 )
 
 func GetMd5Str(str string) string {
@@ -13,4 +16,16 @@ func GetMd5Str(str string) string {
 	gob.NewEncoder(&buf).Encode(str)
 	md5.Write(buf.Bytes())
 	return hex.EncodeToString(md5.Sum(nil))
+}
+
+func GetTokenStr() string {
+	char := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	charArr := strings.Split(char, "")
+	c := len(charArr)
+	ran := rand.New(rand.NewSource(time.Now().Unix()))
+	var str string = ""
+	for i := 1; i <= 18; i++ {
+		str = str + charArr[ran.Intn(c)]
+	}
+	return str
 }
