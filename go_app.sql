e5947a5f18645da7b039427678d2782d7548ad25
yanmx <1712229564@qq.com>
2022-07-07 20:44:35 +0800

    update SQL

--- go_app.sql
+++ go_app.sql
@@ -7,3 +7,39 @@ CREATE TABLE `num_info`
     PRIMARY KEY (`id`, `info_key`)
 ) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
 
+CREATE TABLE `casbin_rule`
+(
+    `p_type` varchar(100) NOT NULL DEFAULT '',
+    `v0`     varchar(100) NOT NULL DEFAULT '',
+    `v1`     varchar(100) NOT NULL DEFAULT '',
+    `v2`     varchar(100) NOT NULL DEFAULT '',
+    `v3`     varchar(100) NOT NULL DEFAULT '',
+    `v4`     varchar(100) NOT NULL DEFAULT '',
+    `v5`     varchar(100) NOT NULL DEFAULT '',
+    KEY      `IDX_casbin_rule_v4` (`v4`),
+    KEY      `IDX_casbin_rule_v5` (`v5`),
+    KEY      `IDX_casbin_rule_p_type` (`p_type`),
+    KEY      `IDX_casbin_rule_v0` (`v0`),
+    KEY      `IDX_casbin_rule_v1` (`v1`),
+    KEY      `IDX_casbin_rule_v2` (`v2`),
+    KEY      `IDX_casbin_rule_v3` (`v3`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `role_info`
+(
+    `id`   int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(50) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user_info`
+(
+    `id`          int(255) NOT NULL AUTO_INCREMENT,
+    `name`        varchar(255) NOT NULL,
+    `login_name`  varchar(255) NOT NULL,
+    `role`        int(255) NOT NULL,
+    `pwd`         varchar(255) NOT NULL,
+    `create_time` datetime     NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
